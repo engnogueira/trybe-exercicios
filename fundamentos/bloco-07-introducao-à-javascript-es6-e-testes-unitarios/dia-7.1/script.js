dd4d20425690eb6847af00d957bdd200dffb6c7f
engnogueira <engnogueira.hotmail.com>
2021-12-03 14:17:11 -0300

    Alteração para Transformar a função para arrow function

--- fundamentos/bloco-07-introducao-à-javascript-es6-e-testes-unitarios/dia-7.1/script.js
+++ fundamentos/bloco-07-introducao-à-javascript-es6-e-testes-unitarios/dia-7.1/script.js
@@ -12,13 +12,13 @@ Modifique as concatenações para template literals . - Ok
 Copie o código abaixo.
  */
 
-const testingScope = (escopo) => {
+const testingScope = escopo => {
   if (escopo === true) {
     let ifScope = "Não devo ser utilizada fora do meu escopo (if)";
     ifScope = ifScope + " ótimo, fui utilizada no escopo !";
     console.log(`${ifScope}`);
   } else {
-    let elseScope = "Não devo ser utilizada fora meu escopo (else)";
+    const elseScope = "Não devo ser utilizada fora meu escopo (else)";
     console.log(`${elseScope}`);
   }
 }
