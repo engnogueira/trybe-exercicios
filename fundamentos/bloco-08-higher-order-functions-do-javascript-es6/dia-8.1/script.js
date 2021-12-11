3b80e1895852f934cefcea02e78054df208cb202
engnogueira <engnogueira.hotmail.com>
2021-12-11 12:26:39 -0300

    Exercicio 1 do dia-8.1

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-8.1/script.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-8.1/script.js
@@ -1,5 +1,5 @@
 // Ao chamar a função doingThings:
-const doingThings = (atividade) => {
+/* const doingThings = (atividade) => {
   console.log(atividade());
 };
 
@@ -9,7 +9,28 @@ const dormir = () => "Partiu dormir!!";
 
 doingThings(acordar);
 doingThings(lanchar);
-doingThings(dormir);
+doingThings(dormir); */
 
 // Ela deve retornar o valor do respectivo parâmetro, neste caso:
 // Acordando!!
+
+/* 1 - Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id . A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com . */
+
+const newEmployees = (newEmail) => {
+  // https://pt.stackoverflow.com/questions/399451/capitalizar-texto-em-javascript-ignorando-abrevia%c3%a7%c3%b5es/399457#399457
+  const capitalize = require('capitalize-pt-br');
+  const employees = {
+    id1: capitalize(newEmail('Pedro Guerra').split("@")[0].split("_").join(' ')), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: capitalize(newEmail('Luiza Drumond').split("@")[0].split("_").join(' ')), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: capitalize(newEmail('Carlos Paiva').split("@")[0].split("_").join(' ')), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  };
+  return employees;
+};
+
+function newEmail(nomeCompleto) {
+  let nome = nomeCompleto.toLowerCase().split(" ").join("_");
+  return `${nome}@trybe.com`
+}
+
+console.log(newEmail('Gilson Nogueira'));
+console.log(newEmployees(newEmail));
