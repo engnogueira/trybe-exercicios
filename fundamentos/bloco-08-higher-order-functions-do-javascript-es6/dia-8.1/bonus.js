bc018134eefa30fbce048f3fac4f98afd432dbf2
engnogueira <engnogueira.hotmail.com>
2021-12-12 17:42:59 -0300

    Exercício 2 da Parte II do bonus do dia-8.1

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-8.1/bonus.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-8.1/bonus.js
@@ -81,11 +81,11 @@ const turnoWarrior = (funcaoDanoWarrior, warrior, dragon) => {
 2 - Crie a segunda HOF que compõe o objeto gameActions . Ela será a função que simula o turno do personagem mage . Esta HOF receberá como parâmetro a função que calcula o dano deferido pelo personagem mage e atualizará os healthPoints do monstro dragon . Além disto ela também deve atualizar o valor das chaves damage e mana do mage.
  */
 
-// const turnoMage = (funcaoDanoMage, funcaoManaMage, mage, dragon) => {
-//   mage.damage = danoMage(mage);
-//   mage.mana -= 15;
-//   dragon.healthPoints -= mage.damage;
-// };
+const turnoMage = (funcaoDanoMage, funcaoManaMage, mage, dragon) => {
+  mage.damage = danoMage(mage);
+  mage.mana -= 15;
+  dragon.healthPoints -= mage.damage;
+};
 
 /* 
 3 - Crie a terceira HOF que compõe o objeto gameActions . Ela será a função que simula o turno do monstro dragon . Esta HOF receberá como parâmetro a função que calcula o dano deferido pelo monstro dragon e atualizará os healthPoints dos personagens mage e warrior . Além disto ela também deve atualizar o valor da chave damage do monstro.
