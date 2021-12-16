4c8a7debcb899594973fe8e409786e43a42e8e58
engnogueira <engnogueira.hotmail.com>
2021-12-16 12:41:35 -0300

    Bônus, atividade 1 dia-7.2

---
+++ fundamentos/bloco-07-introducao-à-javascript-es6-e-testes-unitarios/dia-7.2/bonus-1.js
@@ -0,0 +1,45 @@
+const lesson1 = {
+  materia: "Matemática",
+  numeroEstudantes: 20,
+  professor: "Maria Clara",
+  turno: "manhã",
+};
+
+const lesson2 = {
+  materia: "História",
+  numeroEstudantes: 20,
+  professor: "Carlos",
+};
+
+const lesson3 = {
+  materia: "Matemática",
+  numeroEstudantes: 10,
+  professor: "Maria Clara",
+  turno: "noite",
+};
+
+/* 
+Bônus
+
+1 - Crie uma função para contar quantos estudantes assistiram às aulas de Matemática. Use o objeto criado no exercício 5.
+2 - Crie uma função que deverá retornar um objeto que representa o relatório do professor ou professora, as aulas que ele ou ela ministrou e o número total de estudantes. Use o objeto criado no exercício 5:
+ */
+
+const allLessons = {
+  lesson1: Object.assign({}, lesson1),
+  lesson2: Object.assign({}, lesson2),
+  lesson3: Object.assign({}, lesson3),
+};
+
+const contarEstudantesMatematica = (objeto) => {
+  let total = 0;
+  let keys = Object.keys(objeto);
+  for (let key of keys) {
+    if (objeto[key].materia === "Matemática") {
+      total += objeto[key].numeroEstudantes;
+    }
+  }
+  return total;
+};
+
+console.log(contarEstudantesMatematica(allLessons));
