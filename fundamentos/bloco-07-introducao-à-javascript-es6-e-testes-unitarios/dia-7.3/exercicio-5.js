a32a648bfc32b873d811f1b5c8cdbdfeda37960c
engnogueira <engnogueira.hotmail.com>
2021-12-17 19:58:47 -0300

    Teste 3 do exercicio-5.js dia-7.3

--- fundamentos/bloco-07-introducao-à-javascript-es6-e-testes-unitarios/dia-7.3/exercicio-5.js
+++ fundamentos/bloco-07-introducao-à-javascript-es6-e-testes-unitarios/dia-7.3/exercicio-5.js
@@ -1,6 +1,13 @@
-
 function techList(techs, name) {
-
+  let vetorOrdenado = techs.sort();
+  let vetor = [];
+  for (let index = 0; index < vetorOrdenado.length; index += 1) {
+    vetor.push({
+      tech: vetorOrdenado[index],
+      name: name,
+    });
+  }
+  return vetor;
 }
 
 module.exports = techList;
