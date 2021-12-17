ffc40ebfdd4bda7c06555ab988273baaa2701e9f
engnogueira <engnogueira.hotmail.com>
2021-12-17 11:48:41 -0300

    Teste 2 do exercicio-3.js dia-7.3

--- fundamentos/bloco-07-introducao-à-javascript-es6-e-testes-unitarios/dia-7.3/exercicio-3.test.js
+++ fundamentos/bloco-07-introducao-à-javascript-es6-e-testes-unitarios/dia-7.3/exercicio-3.test.js
@@ -4,4 +4,7 @@ describe("Teste da função myFizzBuzz(num)", () => {
   it('Verificar se a função retorna "fizzbuzz" se o número de entrada fore divisível por 3 e 5', () => {
     expect(myFizzBuzz(15)).toBe("fizzbuzz");
   });
+  it('Verificar se a função retorna "fizz" se o número de entrada fore divisível por 3', () => {
+    expect(myFizzBuzz(9)).toBe("fizz");
+  });
 });
