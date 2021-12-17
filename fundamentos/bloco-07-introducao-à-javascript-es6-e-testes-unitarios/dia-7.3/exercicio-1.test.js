acdba6b328e7514dff0c7d896532799b81d71ef9
engnogueira <engnogueira.hotmail.com>
2021-12-17 01:36:53 -0300

    Teste 2 do exercicio-1.js dia-7.3

--- fundamentos/bloco-07-introducao-à-javascript-es6-e-testes-unitarios/dia-7.3/exercicio-1.test.js
+++ fundamentos/bloco-07-introducao-à-javascript-es6-e-testes-unitarios/dia-7.3/exercicio-1.test.js
@@ -4,4 +4,7 @@ describe('Testes do Exercicio 1 do dia-7.3', () => {
   it('Teste se a soma de 4 + 5 é esperado 9', () =>{
     expect(sum(4, 5)).toBe(9);
   })
+  it('Teste se a soma de 0 + 0 é esperado 0', () =>{
+    expect(sum(0, 0)).toBe(0);
+  })
 })
