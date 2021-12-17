da1aabb756d15713a0929ee8b27ecc4def078b1a
engnogueira <engnogueira.hotmail.com>
2021-12-17 10:01:16 -0300

    Teste 2 do exercicio-2.js dia-7.3

--- fundamentos/bloco-07-introducao-à-javascript-es6-e-testes-unitarios/dia-7.3/exercicio-2.test.js
+++ fundamentos/bloco-07-introducao-à-javascript-es6-e-testes-unitarios/dia-7.3/exercicio-2.test.js
@@ -1,7 +1,10 @@
-const myRemove = require('./exercicio-2');
+const myRemove = require("./exercicio-2");
 
-describe('Testes relativos a função myRemove(arr, item) que retorna uma cópia do array enviado sem o item passado como parâmetro se ele existir', () => {
-  it('Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado', () => {
+describe("Testes relativos a função myRemove(arr, item) que retorna uma cópia do array enviado sem o item passado como parâmetro se ele existir", () => {
+  it("Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado", () => {
     expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
   });
-})
+  it("Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]", () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  });
+});
