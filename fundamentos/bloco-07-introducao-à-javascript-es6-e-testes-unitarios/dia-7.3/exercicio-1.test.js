eb541cff511b4e42a14022368b01a14c39495f40
engnogueira <engnogueira.hotmail.com>
2021-12-17 09:39:17 -0300

    Teste 3 do exercicio-1.js dia-7.3

--- fundamentos/bloco-07-introducao-à-javascript-es6-e-testes-unitarios/dia-7.3/exercicio-1.test.js
+++ fundamentos/bloco-07-introducao-à-javascript-es6-e-testes-unitarios/dia-7.3/exercicio-1.test.js
@@ -1,10 +1,15 @@
-const sum = require('./exercicio-1');
+const sum = require("./exercicio-1");
 
-describe('Testes do Exercicio 1 do dia-7.3', () => {
-  it('Teste se a soma de 4 + 5 é esperado 9', () =>{
+describe("Testes do Exercicio 1 do dia-7.3", () => {
+  it("Teste se a soma de 4 + 5 é esperado 9", () => {
     expect(sum(4, 5)).toBe(9);
-  })
-  it('Teste se a soma de 0 + 0 é esperado 0', () =>{
+  });
+  it("Teste se a soma de 0 + 0 é esperado 0", () => {
     expect(sum(0, 0)).toBe(0);
-  })
-})
+  });
+  it('Teste se a função sum lança um erro quando os parâmetros são 4 e "5"', () => {
+    expect(() => {
+      sum(4, "5");
+    }).toThrow("");
+  });
+});
