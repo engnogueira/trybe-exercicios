dd4591eb2b0134a418e26a7dc366d2623bbdc844
engnogueira <engnogueira.hotmail.com>
2021-12-17 20:39:30 -0300

    Teste se searchEmployee é definida do exercicio bonus.js do dia-7.3

---
+++ fundamentos/bloco-07-introducao-à-javascript-es6-e-testes-unitarios/dia-7.3/bonus.test.js
@@ -0,0 +1,7 @@
+const searchEmployee = require("./bonus");
+
+describe("Testes de implantação de uma função utilizando metodologia TDD", () => {
+  it("Testa se a função searchEmployee é definida", () => {
+    expect(searchEmployee).toBeDefined();
+  });
+});
