d6971a9e3c808290d72624b9732fabb64e88f765
engnogueira <engnogueira.hotmail.com>
2021-12-17 20:40:55 -0300

    Teste se searchEmployee é uma função do exercicio bonus.js do dia-7.3

--- fundamentos/bloco-07-introducao-à-javascript-es6-e-testes-unitarios/dia-7.3/bonus.test.js
+++ fundamentos/bloco-07-introducao-à-javascript-es6-e-testes-unitarios/dia-7.3/bonus.test.js
@@ -4,4 +4,7 @@ describe("Testes de implantação de uma função utilizando metodologia TDD", (
   it("Testa se a função searchEmployee é definida", () => {
     expect(searchEmployee).toBeDefined();
   });
+  it("Testa se searchEmployee é uma função", () => {
+    expect(typeof searchEmployee).toBe('function');
+  });
 });
