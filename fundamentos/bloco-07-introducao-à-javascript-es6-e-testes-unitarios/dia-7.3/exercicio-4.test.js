e341c4542912962cb82a69e57087909ce0188dbf
engnogueira <engnogueira.hotmail.com>
2021-12-17 15:42:11 -0300

    Teste 3 do exercicio-4.js dia-7.3

--- fundamentos/bloco-07-introducao-à-javascript-es6-e-testes-unitarios/dia-7.3/exercicio-4.test.js
+++ fundamentos/bloco-07-introducao-à-javascript-es6-e-testes-unitarios/dia-7.3/exercicio-4.test.js
@@ -7,4 +7,7 @@ describe("encodes relativo as funções encode e decode do projeto Playground Fu
   it("encode para encode se as vogais a, e, i, o, u são convertidas em 1, 2, 3, 4 e 5, respectivamente", () => {
     expect(encode('aeiou')).toBe('12345');
   });
+  it("encode para encode se as vogais a, e, i, o, u são convertidas em 1, 2, 3, 4 e 5, respectivamente", () => {
+    expect(decode('12345')).toBe('aeiou');
+  });
 });
