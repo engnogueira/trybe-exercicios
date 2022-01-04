56430cc014b921f141eebca66af576409e6b05ca
engnogueira <engnogueira.hotmail.com>
2022-01-04 16:52:57 -0300

    fixacao-3 do dia-8.5

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-8.5/fixacao-3.js
@@ -0,0 +1,5 @@
+const dadosTotais = require('./fixacao-2.js');
+
+// console.log('Dados totais: ', dadosTotais);
+
+console.log(`Hi, my name is ${dadosTotais[0]}, I'm ${dadosTotais[1]} years old and I'm ${dadosTotais[2]}. I work as a ${dadosTotais[3]} and my squad is ${dadosTotais[5]}-${dadosTotais[4]}.`);
