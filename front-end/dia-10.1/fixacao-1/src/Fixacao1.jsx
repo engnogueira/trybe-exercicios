ad609e947aa3f245d5a89b5fa8d3b5d8efba3491
engnogueira <engnogueira.hotmail.com>
2022-01-23 21:26:12 -0300

    fixacao-1 do dia-10.1

---
+++ front-end/dia-10.1/fixacao-1/src/Fixacao1.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Fixacao1 extends Component {
+  render() {
+    return <div><h1>Gilson Nogueira</h1><p>Sou natural de Sorocaba/SP, formado em Eng. Civil de pai de 5 belos filhos, gosto muito de estudar e no momento frequento o curso da Trybe na Turma XP/A, fazendo parte do processo de qualificação/seleção de Engenheiros de Software para preencherem quadros da XP Inc. </p></div>
+  }
+}
+
+export default Fixacao1;
